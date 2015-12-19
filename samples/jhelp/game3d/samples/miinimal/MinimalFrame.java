db8316f5721e7c10c5cb00356de2de2c6c759e09
JHelpgg <jhelpgg@gmail.com>
2015-12-19 18:40:58 +0100

    Add license

--- samples/jhelp/game3d/samples/miinimal/MinimalFrame.java
+++ samples/jhelp/game3d/samples/miinimal/MinimalFrame.java
@@ -1,3 +1,13 @@
+/**
+ * <h1>License :</h1> <br>
+ * The following code is deliver as is. I take care that code compile and work, but I am not responsible about any damage it may
+ * cause.<br>
+ * You can use, modify, the code as your need for any usage. But you can't do any action that avoid me or other person use,
+ * modify this code. The code is free for usage and modification, you can't change that fact.<br>
+ * <br>
+ * 
+ * @author JHelp
+ */
 package jhelp.game3d.samples.miinimal;
 
 import java.awt.HeadlessException;
@@ -148,7 +158,7 @@ public class MinimalFrame
             if((System.currentTimeMillis() - this.changeTime) > 2048)
             {
                this.changeTime = System.currentTimeMillis();
-               this.manipulateNode = !this.manipulateNode;
+               this.manipulateNode = !this.manipulateNode || (this.position == null);
             }
          break;
          default:
